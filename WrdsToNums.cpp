b3ff83d6cebd9fc36995bed842d3ac6460970600
lcanseco <79551188+lcanseco@users.noreply.github.com>
2021-02-24 01:55:02 +0100

    Update WrdsToNums.cpp

--- WrdsToNums.cpp
+++ WrdsToNums.cpp
@@ -1,3 +1,10 @@
+/*
+● Write an executable that converts written numbers in words to digits in the middle of a paragraph
+○ Examples
+■ If the input is ‘one hundred and one’, the output should be ‘101’
+■ If the input is ‘I have one hundred apples’ the output should be ‘I have 100
+apples’
+*/
 #include <iostream>
 #include <string>
 #include <map>
